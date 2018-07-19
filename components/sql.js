be1b396bf09af3edf8fdf13b9a03026576f2b09c
hpenav <domotica.humberto@gmail.com>
2018-07-19 12:59:39 -0500

    dev

--- components/sql.js
+++ components/sql.js
@@ -59,7 +59,7 @@ exports.getComponent = function() {
 
           
               var data = null;;
-          	  for(var k=0; k<resultdata.length; k++){
+          	  for(var k=0; k<resultData.length; k++){
                  data = resultData[k]; 
                  console.log(data.AutoNum);
                 
