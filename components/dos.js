5c7c18d8268888e8c2071faeff436243184f7c7d
hpenav <domotica.humberto@gmail.com>
2018-07-26 09:11:32 -0500

    dev

--- components/dos.js
+++ components/dos.js
@@ -26,6 +26,9 @@ exports.getComponent = function() {
     var data = input.getData('in');
     if(data != null) {
     	console.log(">> : " + data.AutoNum);
+        var autoNum = data.AutoNum;
+        var axtraxEmpID = data.iUserNum;
+        r.
     	// Process data and send output
     }
     output.send({
@@ -36,3 +39,33 @@ exports.getComponent = function() {
   });
   return c;
 };
+
+
+
+/*
+Sample Axtrax Event Record:
+
+{ AutoNum: '78267',
+  iEventNum: 817,
+  bDoorNumber: 1,
+  dtSave: 2018-07-20T08:23:04.000Z,
+  dEvent_Date: 2018-07-20T07:54:08.000Z,
+  tByte6: '11',
+  tByte7: '01',
+  tByte8: '21',
+  iUserNum: 56,
+  iSlotNum: 35,
+  tCardCode: '',
+  iReader: 1,
+  iDoor: 0,
+  bCamera: false,
+  iUserSlot: 60,
+  bAlarmHandl: 0,
+  iInput: 0,
+  iOutput: 0,
+  iCamera: 0,
+  tCameraIdent: '',
+  iServerVitrax: 0,
+  Event_Desc: null 
+}
+*/
