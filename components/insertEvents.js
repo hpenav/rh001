09331f2460aece1e9b2da12132d577a45fa3b822
hpenav <domotica.humberto@gmail.com>
2018-07-27 16:36:23 -0500

    dev

--- components/insertEvents.js
+++ components/insertEvents.js
@@ -29,7 +29,8 @@ exports.getComponent = function() {
     }
     // Read packets we need to process
     var data = input.getData('in');
-    console.log("<---------->");
+    
+    console.log("<--------->");
     console.log(data);
     // Process data and send output
     output.send({
